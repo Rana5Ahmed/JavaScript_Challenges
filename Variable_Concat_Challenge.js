c3df0bc625fd206e8e6c93dd8cd198f6788c36e4
Rana Ahmed <raana.elwakeel@gmail.com>
2025-04-27 20:26:10 +0300

    Add files via upload

---
+++ Variable_Concat_Challenge.js
@@ -0,0 +1,7 @@
+    
+    var theTitle = "Elzero", mainDescription = "Elzero web school",
+    todayDate = "25/10"
+    var mainDiv = `<div> <h3>Hello ${theTitle}</h3>
+    <p>${mainDescription}</p>
+    <span>${todayDate}</span></div>`
+    document.write(mainDiv.repeat(4))
